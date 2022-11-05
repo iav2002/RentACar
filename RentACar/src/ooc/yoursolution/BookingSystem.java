5cfd443aa7e049e5014c1c91f690d900c0c042aa
bernigandara <2021283@student.cct.ie>
2022-11-05 20:07:55 +0000

    BookingSystem
    Ignacio I don't know what it is missing but I am getting an error message one I run Drive.java

--- RentACar/src/ooc/yoursolution/BookingSystem.java
+++ RentACar/src/ooc/yoursolution/BookingSystem.java
@@ -6,6 +6,9 @@ package ooc.yoursolution;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import ooc.enums.Make;
 
 /**
  *
@@ -15,7 +18,36 @@ public class BookingSystem implements BookingSystemInterface{
 
     @Override
     public RentACarInterface setupRentACar(BufferedReader in) throws IOException {
-    return null; 
+    
+        String line = in.readLine(); //reading line
+        int i = 0;  //line number
+        String name = "";
+        List<CarInterface> cars = new ArrayList<>(); //stores cars in list
+        
+        while (line != null){
+            if( i == 0){
+                name = line;    //name equals first lline
+            }else{
+                String [] data = line.split(":"); //split the line
+                String make = data[0];
+                int count = Integer.valueOf(data[2]);
+                
+                Car c = new Car();
+                c.setMake(Make.valueOf(make)); //Ignacio, wasn't working becuase it was missing .valueOf()
+                c.setCount(count);       //update the count
+                cars.add(c);
+            }
+            i++;
+            line = in.readLine();   //reading following line
+        }    
+            
+            RentACar rentACar = new RentACar();
+            rentACar.setCars(cars); //set car info
+            rentACar.setName(name); //set the name
+            
+            return rentACar;
     }
     
-}
+} 
+
+    
